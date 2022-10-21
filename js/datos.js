a6dd061d191a43ae2f25ef760757ee6017c89933
Jelena-Ilic <jelenailicsantos@gmail.com>
2022-10-21 21:20:24 +0200

    creación del maquetado en html y de los stylos en css además de incluir el inicio de array de tareas

---
+++ js/datos.js
@@ -0,0 +1,20 @@
+
+var listaTareas = new Array();
+
+listaTareas = [
+    {
+        idTarea: 0,
+        titulo: 'Estudiar JavaScript',
+        prioridad: 'urgente'
+    },
+    {
+        idTarea: 1,
+        titulo: 'Sacar al perro',
+        prioridad: 'diaria'
+    },
+    {
+        idTarea: 3,
+        titulo: 'Salir a comer',
+        prioridad: 'mensual'
+    }
+]
